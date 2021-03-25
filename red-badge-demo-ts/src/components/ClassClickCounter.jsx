dfc627ab6caa81fae049238d61e810f1f7936eaf
Ryan <ryaneckere1@gmail.com>
2021-03-25 19:13:52 -0400

    Conversion Func to Class component

--- red-badge-demo-ts/src/components/ClassClickCounter.jsx
+++ red-badge-demo-ts/src/components/ClassClickCounter.jsx
@@ -1,1 +1,30 @@
 // Create a class version of the Functional Component
+
+import React, { Component } from 'react';
+import { Button, Container } from 'reactstrap';
+
+class FunctionalClickCounter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      countValue: props.initialValue,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ countValue: this.state.countValue - 1 });
+    // setCountValue(countValue - 1);
+  }
+
+  render() {
+    return (
+      <Container>
+        <p>The total clicks left are {this.state.countValue}</p>
+        <Button onClick={this.handleClick}> Click Me From Function</Button>{' '}
+      </Container>
+    );
+  }
+}
+
+export default FunctionalClickCounter;
