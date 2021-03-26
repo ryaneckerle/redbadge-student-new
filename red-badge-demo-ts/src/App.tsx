ace0505f975804c2ed104ec99479562096accb20
Ryan <ryaneckere1@gmail.com>
2021-03-25 21:26:11 -0400

    Convert Func to Class component

--- red-badge-demo-ts/src/App.tsx
+++ red-badge-demo-ts/src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import FunctionFetchOnload from "./components/FunctionFetchOnload";
+import ClassFetchOnload from "./components/ClassFetchOnload"
 
 function App() {
   return (
     <div>
       <FunctionFetchOnload />
+      <hr/>
+      <ClassFetchOnload />
     </div>
   );
 }
