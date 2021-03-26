1aaf9f2ff88ba873e2fd8535f667a2878239682f
Ryan <ryaneckere1@gmail.com>
2021-03-25 20:25:43 -0400

    ClassFetchDeck Component

--- red-badge-demo-ts/src/App.tsx
+++ red-badge-demo-ts/src/App.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import StateExample from './components/StateExample'
+import ClassDeckFetch from './components/ClassDeckFetch'
+
 function App() {
   return (
     <div>
       <StateExample />
+      <ClassDeckFetch />
     </div>
   );
 }
